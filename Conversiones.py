ba5378283c2fc1c9503a91a1c284aaf928a8e1c1
patogc1910 <55264944+patogc1910@users.noreply.github.com>
2019-09-14 10:34:19 -0500

    Create Conversiones.py

---
+++ Conversiones.py
@@ -0,0 +1,26 @@
+Valor = "1234" 
+
+print(type(Valor)) #Gracias al operador type nos ayuda que tipo de valor es entero,decimal etcc...
+
+Valor = int(1234)
+
+print(type(Valor))
+
+Valor = "1234"
+print(len(str(Valor)))
+
+Entrada = "Numero Que Utilizamos {}"
+print(Entrada.format(Valor))
+
+#Jose Patricio Gomez Calderon #1852897
+#Fecha:14/09/2019
+
+
+
+
+
+
+
+
+
+
