01153d1e4e0b867939ca9b856b0952f010088321
patogc1910 <55264944+patogc1910@users.noreply.github.com>
2019-09-20 21:57:50 -0500

    Create Tablas.py

---
+++ Tablas.py
@@ -0,0 +1,133 @@
+Tablas=input("Tabla del: ") # aqui vamos a imprimir el texto , vamos a ponerle el numero que queramos
+Tablas=int(Tablas) #en el operador int le estamos diciendo que que lo vamos a convertir en un numero entero
+
+
+for x in range (1 , 11):
+
+    Multiplicacion= "{} x {} = {} "
+
+    print(Multiplicacion.format(Tablas,x,Tablas*x))
+else:
+    print()
+
+
+Tables=input("Tabla Del: ")  #este codigo nos va el texto y nosotros debemos de poner cual numero de la tabla quieres
+Tables=int(Tables)   #este codigo es el mismo nadamas que estoy haciendo que me de dos tablas
+
+for a in range (1 , 11):
+
+    Multiplicacion= "{} x {} = {} "
+
+    print(Multiplicacion.format(Tables,a,Tables*a)) #aqui estamos imprimiendo el texto del cogido tabla_multiplicar y tambien va imprimir nuestro numero y el numero de la computadora que va a poner
+
+     #este codigo es el mismo nadamas que estoy haciendo que me de dos tablas
+
+
+else:
+    print()
+
+
+Tables=input("Tabla Del: ")
+Tables=int(Tables)
+
+for m in range (1 , 11):
+
+    Multiplication_Table= "{} x {} = {} "
+
+    print(Multiplication_Table.format(Tables,m,Tables*m))
+
+else:
+    print()
+
+Tables=input("Tabla Del: ")
+Tables=int(Tables)
+
+for f in range (1 , 11):
+
+    Multiplication_Table= "{} x {} = {} "
+
+    print(Multiplication_Table.format(Tables,f,Tables*f))
+
+else:
+    print()
+
+Tables=input("Tabla Del: ")
+Tables=int(Tables)
+
+for k in range (1 , 11):
+
+    Multiplication_Table= "{} x {} = {} "
+
+    print(Multiplication_Table.format(Tables,k,Tables*k))
+
+else:
+    print()
+
+Tables=input("Tabla Del: ")
+Tables=int(Tables)
+
+for i in range (1 , 11):
+
+    Multiplication_Table= "{} x {} = {} "
+
+    print(Multiplication_Table.format(Tables,i,Tables*i))
+
+else:
+    print()
+
+Tables=input("Tabla Del: ")
+Tables=int(Tables)
+
+for h in range (1 , 11):
+
+    Multiplication_Table= "{} x {} = {} "
+
+    print(Multiplication_Table.format(Tables,h,Tables*h))
+
+else:
+    print()
+
+
+Tables=input("Tabla Del: ")
+Tables=int(Tables)
+
+for y in range (1 , 11):
+
+    Multiplication_Table= "{} x {} = {} "
+
+    print(Multiplication_Table.format(Tables,y,Tables*y))
+
+else:
+    print()
+
+Tables=input("Tabla Del: ")
+Tables=int(Tables)
+
+for e in range (1 , 11):
+
+    Multiplication_Table= "{} x {} = {} "
+
+    print(Multiplication_Table.format(Tables,e,Tables*e))
+
+else:
+    print()
+
+Tables=input("Tabla Del: ")
+Tables=int(Tables)
+
+for o in range (1 , 11):
+
+    Multiplication_Table= "{} x {} = {} "
+
+    print(Multiplication_Table.format(Tables,o,Tables*o))
+
+else:
+    print()
+
+
+print("VEN,HAZ TUS MULTIPLICACIONES CON ESTE CODIGO XD")
+
+#ES LO MISMO NADAMAS HAY QUE IMPRIMIR TODAS LAS TABLAS QUE QUIERAS PERO NOSOTROS IMPRIMIMOS NADAMAS 10 TABLAS DE MULTIPLICAR 
+
+#Nombre:Jose Patricio Gomez Calderon
+#Fecha:20/09/2019
