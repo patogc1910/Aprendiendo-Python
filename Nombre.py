d7f671ad5430475330b0909cc8ccba48718824b5
patogc1910 <55264944+patogc1910@users.noreply.github.com>
2019-09-20 20:03:46 -0500

    Create Nombre.py

---
+++ Nombre.py
@@ -0,0 +1,15 @@
+name=input("YourName:")             #aqui le estamos pidiendo que pongamos un nombre en minusculas o convinado
+lastname=input("YourLastName:")     #aqui estamos pidiendo que ponga su apellido
+FulName = name + " "+ lastname      # aqui estamos pidiendole que nos de el  nombre completo  y las comillas son para que el nombre completo no este junto, sirve para un espacio
+
+print(FullName.upper())            #el operador.upper hace la opcion de poner todas en mayusculas por ejemplo: el nombre completo nos lo va a dar en mayusculas
+
+
+Nombre:Jose Patricio Gomez Calderon
+Fecha:20/09/2019
+
+
+
+
+
+
